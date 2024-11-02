9a05ee52630f1a95033d0ae2f7d7bf1faa4017b4
MacBook <macbook@MacBooks-MacBook-Air.local>
2024-11-02 16:17:53 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>name: nikolozi pantsulaia</h1>
+      <h1>age: 13</h1>
+      <h1>hobby: anything</h1>
+    </div>
+  )
+}
+
+export default App
